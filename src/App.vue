194ba143d8d340d447e595980f4e385d572c16e5
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-09-23 10:23:31 +0200

    fix: background video and footer relation

--- src/App.vue
+++ src/App.vue
@@ -27,14 +27,19 @@ export default {
 </script>
 
 <template>
+  <div class="video-background-container">
     <!-- Sfondo video -->
     <video autoplay loop muted playsinline id="video-background">
       <source src="./assets/video/luci.mp4" type="video/mp4">
     </video>
+  </div>
+  <div class="content-container">
     <AppHeader></AppHeader>
-    <router-view></router-view>
-    <AppFooter></AppFooter>
+    <router-view class="content" ></router-view>
+    <AppFooter class="footer" ></AppFooter>
+  </div>
 </template>
+
 
 <style lang="scss" >
   @use './style/generic.scss'
